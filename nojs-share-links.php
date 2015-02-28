d41fd1656b003eb63d4e8fc35946365bba913b50
Wojcieh M. Wnuk <dmakabr@grimn.es>
2015-02-28 20:04:55 +0100

    Some changes and more in progress

--- nojs-share-links.php
+++ nojs-share-links.php
@@ -13,22 +13,24 @@ function nojs_share_links()
 {
 
     global $post;
-    $content = get_the_content( $post->ID );
+    //$content = get_the_content( $post->ID );
 
-	if (is_page()) return $content;
+	//if (is_page()) return $content;
 
 	$permalink = esc_url( get_the_permalink( $post->ID ) );
 	$title = get_the_title( $post->ID );
 
     $html = <<<NOJS
-<a href="https://www.facebook.com/sharer/sharer.php?u={$permalink}" target="_blank">Facebook</a>
-<a href="https://twitter.com/home?status={$title}:%20{$permalink}" target="_blank">Twitter</a>
-<a href="https://plus.google.com/share?url={$permalink}" target="_blank">Google+</a>
+    <span>
+    <a href="https://www.facebook.com/sharer/sharer.php?u={$permalink}" target="_blank"><span class="genericon genericon-facebook"></span>Facebook</a>
+    <a href="https://twitter.com/home?status={$title}:%20{$permalink}" target="_blank"><span class="genericon genericon-twitter"></span>Twitter</a>
+    <a href="https://plus.google.com/share?url={$permalink}" target="_blank"><span class="genericon genericon-googleplus"></span>Google+</a>
+    </span><br />
 NOJS;
 
-	return $content . '<hr />' .$html;
+	return $html;
 }
 
-add_action('nojs_share_links', 'nojs_share_links');
+//add_action('nojs_share_links', 'nojs_share_links');
 
-add_filter('the_content', 'nojs_share_links');
+//add_filter('the_content', 'nojs_share_links');
